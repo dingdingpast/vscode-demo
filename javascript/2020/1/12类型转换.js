1db63c05bd2467e51a137c54791d2c341fcf4b10
dingdingpast <58675765+dingdingpast@users.noreply.github.com>
2020-01-15 10:21:18 +0800

    类型转换学习

--- javascript/2020/1/12类型转换.js
+++ javascript/2020/1/12类型转换.js
@@ -22,9 +22,52 @@ function isArray(myArray){
     return myArray.constructor.toString().indexOf('Array') > -1;//检查某个对象是否为数组
 }
 // 。
+//  
+// 全局方法String()把数字转换为字符串。 数字方法toString()同理。可用于任意类型的数字文字变量或表达式。
+// String(x); String(123); String(123+23); 
+// x.toString(); (123).toString(); (123+23).toString(); 
+//  
+// 把布尔转换为字符串
+// String(false) // false.toString();
 // 
-// 全局方法String()把数字转换为字符串。数字方法toString()同理。可用于任意类型的数字文字变量或表达式。
-// 更多转换为字符串的方法: toExponential()对数字舍入，使用指数计数来写。 toFixed()对数字舍入，使用指定位数的小数来写。 toPrecision()把数字写为指定的长度。
-// String(x); String(123); String(123+23);
-// x.toString(); (123).toString(); (123+23).toString();
+// 日期转换为字符串
+// String(Date())//返回当前时间以字符串写
 // 
+// 把字符串转换为数值Number()，空的字符串转换为0，其他字符串转换为NaN（not a number//不是数字）
+// Number('3.14');//返回3.14
+// Number(' ');//返回0
+// Number('88 99')//返回NaN
+// 
+// 一元的 + 运算符用于把变量转换为数字。使用变成数字，无数字可供转换则返回值为NaN
+// var y = '5';// y is string
+// var x = + y;// x is number
+// 
+// 布尔转换为number   number(false);//return 0;   number(true);//return 1;
+// 
+// 日期转换为number   d = new Date(); Number(d);// return Date String;    d = new Date(); d.getTime()// return Time Sting;
+// 
+/*
+原始值         转换为数字	 转换为字符串	     转换为逻辑
+false           0            "false"             false
+true            1            "true"              true
+0               0            "0"                 false
+1               1            "1"                 true
+"0"             0            "0"                 true
+"000"           0            "000"               true
+"1"             1            "1"                 true
+NaN             NaN          "NaN"               false
+Infinity        Infinity     "Infinity"          true
+-Infinity -     Infinity     "-Infinity"         true
+""              0            ""                  false
+"20"            20           "20"                true
+"twenty"        NaN          "twenty"            true
+[]              0            ""                  true
+[20]            20           "20"                true
+[10, 20]        NaN          "10,20"             true
+["twenty"]      NaN          "twenty"            true
+["t","y"]       NaN          "t,y"               true
+function (){}   NaN         "function(){}"       true
+{}              NaN          "[object Object]"   true
+null            0            "null"              false
+undefined       NaN          "undefined"         false
+*/
